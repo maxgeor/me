240f9d1d46d37f701b3f691f39986491f829b004
Max <73665502+maxgeor@users.noreply.github.com>
2022-10-16 00:32:23 +0100

    Added first heros

---
+++ heros/index.js
@@ -0,0 +1,12 @@
+const heros = [
+  {
+    name: 'Gunpei Yokoi',
+    image: 'gunpei.jpeg',
+    link: 'https://en.wikipedia.org/wiki/Gunpei_Yokoi'
+  },
+  {
+    name: 'Issey Miyake',
+    image: 'issey.jpeg',
+    link: 'https://en.wikipedia.org/wiki/Issey_Miyake'
+  }
+];
